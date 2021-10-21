c4a19852f4174bd13af94f29f5acca76721714ea
xtacodeperro3x <92846124+xtacodeperro3x@users.noreply.github.com>
2021-10-20 23:07:18 -0600

    Update index.html

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn')
 
 yesBtn.addEventListener('click',function () {
-    alert('Siempre supe que dirias que si culona :) ');
+    alert('Siempre supe que dirias que si :) ');
 })
 yesBtn.style.background = 'teal';
 
